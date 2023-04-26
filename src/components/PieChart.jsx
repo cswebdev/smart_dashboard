66073e3211efacbb8718962d9c5f51e3be011a8c
cswebdev <csnider91@live.com>
2023-04-26 13:31:24 -0400

    updated pie chart styling. Removed the fill decorations. Updated text color

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -54,7 +54,7 @@ const PieChart = () => {
                 ]
             }}
             arcLinkLabelsSkipAngle={10}
-            arcLinkLabelsTextColor="#333333"
+            arcLinkLabelsTextColor={colors.grey[100]}
             arcLinkLabelsThickness={2}
             arcLinkLabelsColor={{ from: 'color' }}
             enableArcLabels={false}
@@ -88,56 +88,7 @@ const PieChart = () => {
                     spacing: 10
                 }
             ]}
-            fill={[
-                {
-                    match: {
-                        id: 'ruby'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'c'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'go'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'python'
-                    },
-                    id: 'dots'
-                },
-                {
-                    match: {
-                        id: 'scala'
-                    },
-                    id: 'lines'
-                },
-                {
-                    match: {
-                        id: 'lisp'
-                    },
-                    id: 'lines'
-                },
-                {
-                    match: {
-                        id: 'elixir'
-                    },
-                    id: 'lines'
-                },
-                {
-                    match: {
-                        id: 'javascript'
-                    },
-                    id: 'lines'
-                }
-            ]}
+
             legends={[
                 {
                     anchor: 'bottom',
