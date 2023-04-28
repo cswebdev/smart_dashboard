e17d62c3241ceb95ff9de9d43a5a4e9f7ee1c46e
Chelsea S <csnider91@live.com>
2023-04-28 15:28:52 -0400

    adding index tag to browser router

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
                <main className="content">
                   <Topbar setIsSidebar={setIsSidebar} />
                   <Routes>
-                     <Route path="/" element={<Dashboard />}>
+                     <Route path="/" index element={<Dashboard />}>
                         <Route path="/team" element={<Team />} />
                         <Route path="/contacts" element={<Contacts />} />
                         <Route path="/invoices" element={<Invoices />} />
