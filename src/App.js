78177934bcd8cf2a9b0ab82edcec851235b4acdf
Chelsea S <csnider91@live.com>
2023-04-28 15:32:39 -0400

    testing browser router gh page index

--- src/App.js
+++ src/App.js
@@ -29,18 +29,18 @@ function App() {
                <main className="content">
                   <Topbar setIsSidebar={setIsSidebar} />
                   <Routes>
-                     <Route path="/" index element={<Dashboard />}>
-                        <Route path="/team" element={<Team />} />
-                        <Route path="/contacts" element={<Contacts />} />
-                        <Route path="/invoices" element={<Invoices />} />
-                        <Route path="/form" element={<Form />} />
-                        <Route path="/bar" element={<Bar />} />
-                        <Route path="/pie" element={<Pie />} />
-                        <Route path="/line" element={<Line />} />
-                        <Route path="/faq" element={<FAQ />} />
-                        <Route path="/calendar" element={<Calendar />} />
-                        <Route path="/geography" element={<Geography />} />
-                     </Route>
+                     <Route path="/" element={<Dashboard />} />
+                     <Route path="/team" element={<Team />} />
+                     <Route path="/contacts" element={<Contacts />} />
+                     <Route path="/invoices" element={<Invoices />} />
+                     <Route path="/form" element={<Form />} />
+                     <Route path="/bar" element={<Bar />} />
+                     <Route path="/pie" element={<Pie />} />
+                     <Route path="/line" element={<Line />} />
+                     <Route path="/faq" element={<FAQ />} />
+                     <Route path="/calendar" element={<Calendar />} />
+                     <Route path="/geography" element={<Geography />} />
+                     <Route path="*" index element={<Dashboard />} />
                   </Routes>
                </main>
             </div>
